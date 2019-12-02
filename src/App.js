7e815394632349ca5fd5961356b2f4c4748aa2ad
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-12-02 14:51:51 -0500

    added info to app file

--- src/App.js
+++ src/App.js
@@ -4,6 +4,30 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos: data
+    }
+  }
+
+  addTodo = name => {
+    const newTodo = {
+      name: name,
+      id: Date.now(),
+      purchased: false
+    }
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  };
+
+  clearPurchased = () => {
+    this.setState({
+      todos: this.state.todos.filter(item => !item.purchased)
+    })
+  }
+  
   render() {
     return (
       <div>
