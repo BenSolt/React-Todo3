60b7cb83d834002a9f45bc310a9752373dddb792
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-12-02 15:11:19 -0500

    added todo form file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import React from 'react';
 
+const data = [
+  {
+    name:'Start',
+    id:1,
+    purchased: false
+  },
+  {
+    name:'second',
+    id:2,
+    purchased: false
+  },
+  {
+    name:'third',
+    id:3,
+    purchased: false
+  }
+
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
@@ -25,6 +44,21 @@ class App extends React.Component {
   clearPurchased = () => {
     this.setState({
       todos: this.state.todos.filter(item => !item.purchased)
+    })
+  }
+
+  togglePurchased = id => {
+    this.setState({
+      todos: this.state.todos.map(item => {
+        if(item.id === id) {
+          return {
+            ...item,
+            purchased: !item.purchased
+          }
+        }else{
+          return item;
+        }
+      })
     })
   }
   
